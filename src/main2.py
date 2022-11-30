2617bd03cc0ed17f8ab54ea20d82c89f6b23ad8c
Daniel Du <danieldu109@gmail.com>
2022-11-30 17:40:16 +0000

    menu

--- src/main2.py
+++ src/main2.py
@@ -1,16 +1,33 @@
-import 
+from interface import Interface
 
-print(
-"""
-1 - Carregar mapa 1
-2 - Carregar mapa 2
-3 - Carregar mapa 3
-4 - Carregar mapa 4
-5 - Carregar mapa 5
-""")
-x = int(input())
-    
-path = f"../pistas/pista{x}.txt"
+def main():
+    saida = -1
 
-
-
+    while saida != 0:
+        Interface.mainMenu()
+        saida = Interface.pedeInput()
+        if saida == 0:
+            Interface.sair()
+        elif saida == 1:
+            i = -1
+            while i != 0:
+                Interface.jogarMenu()
+                i = Interface.pedeInput()
+                if i == 0:
+                    Interface.voltarMenuPrincipal()
+                elif(i == 1 or i == 2 or i == 3 or i == 4): 
+                    e = -1
+                    while e!=0:
+                        Interface.pistaMenu(str(i))
+                        e=Interface.pedeInput()
+                        
+                        # depos é implementar as merdas
+                        if e == 1:
+                            print("ola")
+                else:
+                    Interface.entradaInvalida()
+        else: 
+            Interface.entradaInvalida()
+        
+if __name__ == "__main__":
+    main()
--- src/vetorrace.py
+++ src/vetorrace.py
@@ -3,258 +3,261 @@ from grafo import Grafo
 from node import Node
 from queue import Queue
 from carro import Carro
-
-#devolve uma lista de listas
-def pista(path:str):
-    pista = []
-    f = open(path, "r")
-    conteudo = f.read()
-    #conteudo.split('\n')
-    conteudo = conteudo.splitlines()
-    
-    for linha in conteudo:
-        aux = []
-        aux[:0] = linha
-        pista.append(aux)
-    f.close()
-    return pista
-
-# devolde as posições de um carater na pista
-def charPosition(pista,char):
-    pos = []
-    largura = len(pista[0])
-    altura  = len(pista)
-
-    for x in range(largura):
-        for y in range(altura):
-            if pista[y][x] == char:
-                pos.append((x,y))
-    return pos
-
-#todas as acelarações possíveis
-def acel():
-    return [(-1,-1),(-1,0),(-1,1),(0,-1),(0,0),(0,1),(1,-1),(1,0),(1,1)]
-
-#próximas posiçoes possiveis para um carro! 
-#devolve uma lista de novos carros
-def nextestado(pista,carro,end):
-    l = []
-
-    posi = carro.pos
-    veli = carro.vel
-
-    largura = len(pista[0]) -1
-    altura  = len(pista) -1
-
-    #end variavel global
-    if (posi in end):
-        l.append((carro,1))
-
-    for acele in acel():
-        novoCarro = Carro(posi,veli)
-        novoCarro.nextPosicao(acele)
-
-        x,y = novoCarro.pos
+ 
+class Vetorrace():
+    def __init__(self):
         
-        if not (x < 0 or x > largura or y < 0 or y > altura):
-            #f = temParede(carro,novoCarro)
-            f = intersetaParede(pista,carro,novoCarro)
-
-            if f == False: # or (getChar(pista,(x,y)) == 'X'):
-                novoCarro.setPos(posi)
-                novoCarro.setVel((0,0))
-                if not ((novoCarro,25) in l):
-                    l.append((novoCarro,25))
+    #devolve uma lista de listas
+    def pista(path:str):
+        pista = []
+        f = open(path, "r")
+        conteudo = f.read()
+        #conteudo.split('\n')
+        conteudo = conteudo.splitlines()
+        
+        for linha in conteudo:
+            aux = []
+            aux[:0] = linha
+            pista.append(aux)
+        f.close()
+        return pista
+    
+    # devolde as posições de um carater na pista
+    def charPosition(pista,char):
+        pos = []
+        largura = len(pista[0])
+        altura  = len(pista)
+    
+        for x in range(largura):
+            for y in range(altura):
+                if pista[y][x] == char:
+                    pos.append((x,y))
+        return pos
+    
+    #todas as acelarações possíveis
+    def acel():
+        return [(-1,-1),(-1,0),(-1,1),(0,-1),(0,0),(0,1),(1,-1),(1,0),(1,1)]
+    
+    #próximas posiçoes possiveis para um carro! 
+    #devolve uma lista de novos carros
+    def nextestado(pista,carro,end):
+        l = []
+    
+        posi = carro.pos
+        veli = carro.vel
+    
+        largura = len(pista[0]) -1
+        altura  = len(pista) -1
+    
+        #end variavel global
+        if (posi in end):
+            l.append((carro,1))
+    
+        for acele in acel():
+            novoCarro = Carro(posi,veli)
+            novoCarro.nextPosicao(acele)
+    
+            x,y = novoCarro.pos
+            
+            if not (x < 0 or x > largura or y < 0 or y > altura):
+                #f = temParede(carro,novoCarro)
+                f = intersetaParede(pista,carro,novoCarro)
+    
+                if f == False: # or (getChar(pista,(x,y)) == 'X'):
+                    novoCarro.setPos(posi)
+                    novoCarro.setVel((0,0))
+                    if not ((novoCarro,25) in l):
+                        l.append((novoCarro,25))
+                else:
+                    l.append((novoCarro,1))
+        return l
+    
+    def getChar(pista, coord):
+        x,y = coord
+        return pista[y][x]
+    
+    
+    def geraGrafo(pista,carro,end):
+        # grafo é direcionado
+        g = Grafo(True)
+        v = set() #visited
+        q = Queue()
+        q.put(carro)
+    
+        while not q.empty():
+            #estado
+            e = q.get()
+            le = nextestado(pista,e,end)
+    
+            for car,w in le:
+                g.add_edge(e ,car,w) 
+                if (car not in v):
+                    q.put(car)
+            v.add(e)
+    
+        return g
+    
+    
+    def endcarro(end):
+        l = []
+        for pos in end:
+            novoCarro = Carro(pos)
+            l.append(novoCarro)
+        return l
+    
+    #NÂO USAMOS ISTO
+    def getDeclive(CarroEstadoAtual, CarroEstadoSeguinte):
+    
+        x_CarroAtual, y_CarroAtual = CarroEstadoAtual.getPos()
+        x_CarroSeguinte, y_CarroSeguinte = CarroEstadoSeguinte.getPos()
+        
+        if (x_CarroSeguinte - x_CarroAtual) == 0:
+            return 0
+        declive = (y_CarroSeguinte - y_CarroAtual) / (x_CarroSeguinte - x_CarroAtual)
+        
+        return declive
+        
+    def getOrdenadaOrigem(CarroEstadoAtual, declive):
+        x_CarroAtual,y_CarroAtual = CarroEstadoAtual.getPos()
+        ordenadaOrigem = y_CarroAtual-declive*x_CarroAtual
+        return ordenadaOrigem
+        
+    def verifica_parede_no_meio(lista_de_paredes, declive, ordenadaOrigem,carro1,carro2):
+        tem_parede = False # se devolver falso, é porque nao tem parede no meio
+        for coordx,coordy in lista_de_paredes:
+            cx,cy   = carro1.getPos()
+            cx2,cy2 = carro2.getPos()
+    
+            if ((cx > cx2 and coordx > cx2 and coordx < cx) or (cx2 > cx and coordx > cx  and coordx < cx2)) and ((cy > cy2 and coordy > cy2 and coordy < cy) or (cy2 > cy and coordy > cy  and coordy < cy2)):
+                if coordy == int(declive*coordx + ordenadaOrigem):
+                    tem_parede = True
+    
+                    #print("o = ",ordenadaOrigem)
+                    #print("d = ",declive)
+                    #print("f = ",tem_parede)
+                    return tem_parede
+    
+    
+    def temParede (carro,novoCarro):
+        #declive
+        d = getDeclive(carro,novoCarro)
+        #ordnada na origem
+        o = getOrdenadaOrigem(novoCarro, d)
+    
+        f = verifica_parede_no_meio(lp ,d,o,carro,novoCarro )
+        return f
+    
+    def intersetaParede(pista,c1,c2):
+        c1x,c1y = c1.getPos()    
+        c2x,c2y = c2.getPos()    
+    
+        if (c1x > c2x):
+            aux = Carro((c1x,c1y))
+            c1 = c2
+            c2 = aux
+    
+        c1x,c1y = c1.getPos()    
+        c11x,c11y = c1.getPos()    
+        c2x,c2y = c2.getPos()    
+        c22x,c22y = c2.getPos()    
+        
+        ts = [(c1x,c1y)]
+        ti = [(c2x,c2y)]
+        #ts = []
+        #ti = []
+    
+        if (c1x <= c2x):
+            if (c2y >= c1y):
+                while c1x != c2x:
+                    c1x += 1
+                    ts.append((c1x,c1y))
+                while c1y != c2y:
+                    c1y += 1
+                    ts.append((c1x,c1y))
+                while c11y != c22y:
+                    c11y += 1
+                    ti.append((c11x,c11y))
+                while c11x != c22x:
+                    c11x += 1
+                    ti.append((c11x,c11y))
             else:
-                l.append((novoCarro,1))
-    return l
-
-def getChar(pista, coord):
-    x,y = coord
-    return pista[y][x]
-
-
-def geraGrafo(pista,carro,end):
-    # grafo é direcionado
-    g = Grafo(True)
-    v = set() #visited
-    q = Queue()
-    q.put(carro)
-
-    while not q.empty():
-        #estado
-        e = q.get()
-        le = nextestado(pista,e,end)
-
-        for car,w in le:
-            g.add_edge(e ,car,w) 
-            if (car not in v):
-                q.put(car)
-        v.add(e)
-
-    return g
-
-
-def endcarro(end):
-    l = []
-    for pos in end:
-        novoCarro = Carro(pos)
-        l.append(novoCarro)
-    return l
-
-#NÂO USAMOS ISTO
-def getDeclive(CarroEstadoAtual, CarroEstadoSeguinte):
-
-    x_CarroAtual, y_CarroAtual = CarroEstadoAtual.getPos()
-    x_CarroSeguinte, y_CarroSeguinte = CarroEstadoSeguinte.getPos()
+                while c1x != c2x:
+                    c1x += 1
+                    ti.append((c1x,c1y))
+                while c1y != c2y:
+                    c1y -= 1
+                    ti.append((c1x,c1y))
+                while c11y != c22y:
+                    c11y -= 1
+                    ts.append((c11x,c11y))
+                while c11x != c22x:
+                    c11x += 1
+                    ts.append((c11x,c11y))
     
-    if (x_CarroSeguinte - x_CarroAtual) == 0:
-        return 0
-    declive = (y_CarroSeguinte - y_CarroAtual) / (x_CarroSeguinte - x_CarroAtual)
+        f  = False
+        f2 = False
+        for x,y in ts:
+            if (pista[y][x] == 'X'):
+                f = True
+                break
+            
+        for x,y in ti:
+            if (pista[y][x] == 'X'):
+                f2 = True
+                break
     
-    return declive
+        # posso passar?
+        return (not f) or (not f2)
     
-def getOrdenadaOrigem(CarroEstadoAtual, declive):
-    x_CarroAtual,y_CarroAtual = CarroEstadoAtual.getPos()
-    ordenadaOrigem = y_CarroAtual-declive*x_CarroAtual
-    return ordenadaOrigem
+    def pp(pista,solve):
+        for carros in solve:
+            x,y = carros.getPos()
+            pista[y][x] = '•'
+        for linha in pista:
+            print("".join(linha))
+            #print('\n\n\n\n\n\n\n\n')
+            #time.sleep(0.3)
     
-def verifica_parede_no_meio(lista_de_paredes, declive, ordenadaOrigem,carro1,carro2):
-    tem_parede = False # se devolver falso, é porque nao tem parede no meio
-    for coordx,coordy in lista_de_paredes:
-        cx,cy   = carro1.getPos()
-        cx2,cy2 = carro2.getPos()
-
-        if ((cx > cx2 and coordx > cx2 and coordx < cx) or (cx2 > cx and coordx > cx  and coordx < cx2)) and ((cy > cy2 and coordy > cy2 and coordy < cy) or (cy2 > cy and coordy > cy  and coordy < cy2)):
-            if coordy == int(declive*coordx + ordenadaOrigem):
-                tem_parede = True
-
-                #print("o = ",ordenadaOrigem)
-                #print("d = ",declive)
-                #print("f = ",tem_parede)
-                return tem_parede
-
-
-def temParede (carro,novoCarro):
-    #declive
-    d = getDeclive(carro,novoCarro)
-    #ordnada na origem
-    o = getOrdenadaOrigem(novoCarro, d)
-
-    f = verifica_parede_no_meio(lp ,d,o,carro,novoCarro )
-    return f
-
-def intersetaParede(pista,c1,c2):
-    c1x,c1y = c1.getPos()    
-    c2x,c2y = c2.getPos()    
-
-    if (c1x > c2x):
-        aux = Carro((c1x,c1y))
-        c1 = c2
-        c2 = aux
-
-    c1x,c1y = c1.getPos()    
-    c11x,c11y = c1.getPos()    
-    c2x,c2y = c2.getPos()    
-    c22x,c22y = c2.getPos()    
     
-    ts = [(c1x,c1y)]
-    ti = [(c2x,c2y)]
-    #ts = []
-    #ti = []
-
-    if (c1x <= c2x):
-        if (c2y >= c1y):
-            while c1x != c2x:
-                c1x += 1
-                ts.append((c1x,c1y))
-            while c1y != c2y:
-                c1y += 1
-                ts.append((c1x,c1y))
-            while c11y != c22y:
-                c11y += 1
-                ti.append((c11x,c11y))
-            while c11x != c22x:
-                c11x += 1
-                ti.append((c11x,c11y))
-        else:
-            while c1x != c2x:
-                c1x += 1
-                ti.append((c1x,c1y))
-            while c1y != c2y:
-                c1y -= 1
-                ti.append((c1x,c1y))
-            while c11y != c22y:
-                c11y -= 1
-                ts.append((c11x,c11y))
-            while c11x != c22x:
-                c11x += 1
-                ts.append((c11x,c11y))
-
-    f  = False
-    f2 = False
-    for x,y in ts:
-        if (pista[y][x] == 'X'):
-            f = True
-            break
-        
-    for x,y in ti:
-        if (pista[y][x] == 'X'):
-            f2 = True
-            break
-
-    # posso passar?
-    return (not f) or (not f2)
-
-def pp(pista,solve):
-    for carros in solve:
-        x,y = carros.getPos()
-        pista[y][x] = '•'
-    for linha in pista:
-        print("".join(linha))
-        #print('\n\n\n\n\n\n\n\n')
-        #time.sleep(0.3)
-
-
-
-def corre (path):
-    p = pista(path)
-    start = charPosition (p,"P") [0]
-    c = Carro(start)
-    end = charPosition (p,"F")
-    print("Gerando grafo")
-    g = geraGrafo(p,c,end)
-    print("Grafo gerado")
-    print(
-"""
-Escolhe um algoritmo de procura
-1 - BFS
-2 - DFS
-""")
-    x = int(input())
-    if x == 1:
-        print("BFS")
-        solve,w = g.procuraBFS(c,end)
-    if x == 2:
-        print("DFS")
-        solve,w = g.procuraDFS(c,end)
-        #[print(str(u)) for u in solve]
-    pp(p,solve)
-    print("custo =",w)
-
-
-#p = pista("../pistas/pista2.txt")
-#start = charPosition (p,"P") [0]
-#c = Carro(start)
-#Lista de paredes #VAR GLOBAL 
-#lp = charPosition(p,'X')
-#end = endcarro(charPosition (p,"F")) #VARIAVEL GLOBAL 
-#end = charPosition (p,"F")
-#g = geraGrafo(p,c)
-
-
-#print("DFS")
-#solve,w = g.procuraDFS(c,end)
-#[print(str(u)) for u in solve]
-#print("custo =",w)
-#pp(p,solve)
+    
+    def corre (path):
+        p = pista(path)
+        start = charPosition (p,"P") [0]
+        c = Carro(start)
+        end = charPosition (p,"F")
+        print("Gerando grafo")
+        g = geraGrafo(p,c,end)
+        print("Grafo gerado")
+        print(
+    """
+    Escolhe um algoritmo de procura
+    1 - BFS
+    2 - DFS
+    """)
+        x = int(input())
+        if x == 1:
+            print("BFS")
+            solve,w = g.procuraBFS(c,end)
+        if x == 2:
+            print("DFS")
+            solve,w = g.procuraDFS(c,end)
+            #[print(str(u)) for u in solve]
+        pp(p,solve)
+        print("custo =",w)
+    
+    
+    #p = pista("../pistas/pista2.txt")
+    #start = charPosition (p,"P") [0]
+    #c = Carro(start)
+    #Lista de paredes #VAR GLOBAL 
+    #lp = charPosition(p,'X')
+    #end = endcarro(charPosition (p,"F")) #VARIAVEL GLOBAL 
+    #end = charPosition (p,"F")
+    #g = geraGrafo(p,c)
+    
+    
+    #print("DFS")
+    #solve,w = g.procuraDFS(c,end)
+    #[print(str(u)) for u in solve]
+    #print("custo =",w)
+    #pp(p,solve)
